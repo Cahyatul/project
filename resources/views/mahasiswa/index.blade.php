afa59228cd6524bc71408d462592dc06c988fb3b
Cahyatul <cahyatulanna57372@gmail.com>
2023-06-03 15:04:15 +0700

    firts commit

---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,18 @@
+<table border="1">
+<tr>
+    <th>No</th>
+    <th>Nama</th>
+    <th>Nim</th>
+    <th>Jenis kelamin</th>
+    <th>Alamat</th>
+</tr>
+@foreach($mahasiswa as $m)
+<tr>
+    <td>{{$m->id}}</td>
+    <td>{{$m->nama}}</td>
+    <td>{{$m->nim}}</td>
+    <td>{{$m->jenis_kelamin}}</td>
+    <td>{{$m->alamat}}</td>
+</tr>
+@endforeach
+</table>
