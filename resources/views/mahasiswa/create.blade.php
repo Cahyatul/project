b397c82236cd33cf7804d12e3229d04fcebe4629
Cahyatul <cahyatulanna57372@gmail.com>
2023-06-09 11:12:07 +0700

    boostrap

--- resources/views/mahasiswa/create.blade.php
+++ resources/views/mahasiswa/create.blade.php
@@ -1,14 +1,34 @@
+@extends('layout.master')
+
+@section('content')
+<div class="container">
 <h1>Create Mahasiswa</h1>
 
 <form action="/mahasiswa/store" method="POST">
-    @csrf
-    <input type="text" name="nama" placeholder="nama"><br>
-    <input type="text" name="nim" placeholder="nim"><br>
-    <select name="jenis_kelamin">
+@csrf
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Nama</label>
+        <input type="text" name="nama" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">NIM</label>
+        <input type="text" name="nim" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>
+    
+
+    <select class="form-select" name="jenis_kelamin">
     <option value="">Pilih Jenis Kelamin</option>
     <option value="L">Laki-Laki</option>
     <option value="P">Perempuan</option>
     </select><br>
-    <textarea name="alamat" rows="10"></textarea><br>
-    <input type="submit" name="submit" value="save">
+    
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Alamat</label>
+        <textarea class="form-control" name="alamat" rows="10"></textarea><br>
+    </div>
+
+    <input type="submit" name="submit" class="btn btn-info" value="save">
+
 </form>
+</div>
+@endsection
--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -1,28 +1,37 @@
-<a href="/mahasiswa/create">Add Mahasiswa</a>
-<table border="1">
-<tr>
-    <th>No</th>
-    <th>Nama</th>
-    <th>Nim</th>
-    <th>Jenis kelamin</th>
-    <th>Alamat</th>
-    <th>Aksi</th>
-</tr>
-@foreach($mahasiswa as $m)
-<tr>
-    <td>{{$m->id}}</td>
-    <td>{{$m->nama}}</td>
-    <td>{{$m->nim}}</td>
-    <td>{{$m->jenis_kelamin}}</td>
-    <td>{{$m->alamat}}</td>
-    <td>
-        <a href="/mahasiswa/{{$m->id}}/edit">Edit</a>
-        <form action="/mahasiswa/{{$m->id}}" method="POST">
-            @csrf
-            @method('delete')
-            <input type="submit" value="Delete">
-        </form>
-    </td>
-</tr>
-@endforeach
-</table>
+@extends('layout.master')
+
+@section('content')
+    <div class="container">
+            <a class="btn btn-primary"  href="/mahasiswa/create">Add Mahasiswa</a>
+        <table class="table table-success table-striped">
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Nim</th>
+            <th>Jenis kelamin</th>
+            <th>Alamat</th>
+            <th>Aksi</th>
+        </tr>
+        @foreach($mahasiswa as $m)
+        <tr>
+            <td>{{$m->id}}</td>
+            <td>{{$m->nama}}</td>
+            <td>{{$m->nim}}</td>
+            <td>{{$m->jenis_kelamin}}</td>
+            <td>{{$m->alamat}}</td>
+            <td>
+                <div class="btn-group" role="group" aria-label="Basic example">
+                <a class="btn btn-warning" href="/mahasiswa/{{$m->id}}/edit">Edit</a>
+                <form action="/mahasiswa/{{$m->id}}" method="POST">
+                    @csrf
+                    @method('delete')
+                    <input class="btn btn-danger" type="submit" value="Delete">
+                </div>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+        </table>
+    </div>
+    @endsection
+
